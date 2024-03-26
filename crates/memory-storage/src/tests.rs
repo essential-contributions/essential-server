984b5fc9d91fe928720f7e884a61660428ff4648
Tom <tomrgowan@gmail.com>
2024-03-26 16:57:39 +1100

    testing paging
    more tests
    
    finish tests

--- crates/memory-storage/src/tests.rs
+++ crates/memory-storage/src/tests.rs
@@ -1,16 +1,273 @@
+use std::vec;
+
 use super::*;
-use test_utils::{empty_intent, sign_with_random_keypair};
+use test_utils::{
+    intent_with_vars, partial_solution_with_vars, sign_with_random_keypair, solution_with_vars,
+};
+use utils::hash;
 
 #[tokio::test]
-#[ignore]
 async fn test_insert_intent_set() {
     let storage = MemoryStorage::new();
     let storage_layout = StorageLayout {};
-    let intent = sign_with_random_keypair(vec![empty_intent()]);
+    let intents = [
+        sign_with_random_keypair(vec![
+            intent_with_vars(0),
+            intent_with_vars(1),
+            intent_with_vars(2),
+        ]),
+        sign_with_random_keypair(vec![
+            intent_with_vars(2),
+            intent_with_vars(3),
+            intent_with_vars(4),
+        ]),
+    ];
     storage
-        .insert_intent_set(storage_layout, intent)
+        .insert_intent_set(storage_layout.clone(), intents[0].clone())
         .await
         .unwrap();
+
+    storage
+        .insert_intent_set(storage_layout.clone(), intents[0].clone())
+        .await
+        .unwrap();
+
     let result = storage.list_intent_sets(None, None).await.unwrap();
-    assert_eq!(result, vec![vec![empty_intent()]]);
+    assert_eq!(result, vec![intents[0].data.clone()]);
+
+    storage
+        .insert_intent_set(storage_layout, intents[1].clone())
+        .await
+        .unwrap();
+
+    let result = storage.list_intent_sets(None, None).await.unwrap();
+    assert_eq!(
+        result,
+        vec![intents[0].data.clone(), intents[1].data.clone()]
+    );
+
+    for intent in &intents {
+        for j in 0..3 {
+            let address = IntentAddress {
+                set: ContentAddress(hash(&intent.data)),
+                intent: ContentAddress(hash(&intent.data[j])),
+            };
+
+            let result = storage.get_intent(&address).await.unwrap().unwrap();
+
+            assert_eq!(result, intent.data[j]);
+        }
+    }
+
+    let result = storage
+        .get_storage_layout(&ContentAddress(hash(&intents[0].data)))
+        .await
+        .unwrap()
+        .unwrap();
+    assert_eq!(result, StorageLayout {});
+}
+
+#[tokio::test]
+async fn test_solutions() {
+    let storage = MemoryStorage::new();
+    let solution = sign_with_random_keypair(solution_with_vars(0));
+    let solution2 = sign_with_random_keypair(solution_with_vars(1));
+    let solution3 = sign_with_random_keypair(solution_with_vars(2));
+
+    // Idempotent insert
+    storage
+        .insert_solution_into_pool(solution.clone())
+        .await
+        .unwrap();
+    storage
+        .insert_solution_into_pool(solution.clone())
+        .await
+        .unwrap();
+
+    let result = storage.list_solutions_pool().await.unwrap();
+    assert_eq!(result, vec![solution.clone()]);
+
+    storage
+        .insert_solution_into_pool(solution2.clone())
+        .await
+        .unwrap();
+    let result = storage.list_solutions_pool().await.unwrap();
+    assert_eq!(result.len(), 2);
+    assert!(result.contains(&solution));
+    assert!(result.contains(&solution2));
+
+    storage
+        .move_solutions_to_solved(&[hash(&solution.data)])
+        .await
+        .unwrap();
+
+    let result = storage.list_solutions_pool().await.unwrap();
+    assert_eq!(result.len(), 1);
+    assert!(result.contains(&solution2));
+
+    let result = storage.list_winning_blocks(None, None).await.unwrap();
+    assert_eq!(result.len(), 1);
+    assert_eq!(result[0].batch.solutions, vec![solution.clone()]);
+
+    storage
+        .insert_solution_into_pool(solution3.clone())
+        .await
+        .unwrap();
+
+    storage
+        .move_solutions_to_solved(&[hash(&solution2.data), hash(&solution3.data)])
+        .await
+        .unwrap();
+
+    let result = storage.list_solutions_pool().await.unwrap();
+    assert!(result.is_empty());
+
+    let result = storage.list_winning_blocks(None, None).await.unwrap();
+    assert_eq!(result.len(), 2);
+    assert_eq!(result[0].batch.solutions, vec![solution.clone()]);
+    assert_eq!(
+        result[1].batch.solutions,
+        vec![solution2.clone(), solution3.clone()]
+    );
+}
+
+#[tokio::test]
+async fn test_partial_solutions() {
+    let storage = MemoryStorage::new();
+    let partial_solution1 = sign_with_random_keypair(partial_solution_with_vars(0));
+    let partial_solution2 = sign_with_random_keypair(partial_solution_with_vars(1));
+    let partial_solution3 = sign_with_random_keypair(partial_solution_with_vars(2));
+
+    // Idempotent insert
+    storage
+        .insert_partial_solution_into_pool(partial_solution1.clone())
+        .await
+        .unwrap();
+    storage
+        .insert_partial_solution_into_pool(partial_solution1.clone())
+        .await
+        .unwrap();
+
+    let result = storage.list_partial_solutions_pool().await.unwrap();
+    assert_eq!(result, vec![partial_solution1.clone()]);
+
+    storage
+        .insert_partial_solution_into_pool(partial_solution2.clone())
+        .await
+        .unwrap();
+    let result = storage.list_partial_solutions_pool().await.unwrap();
+    assert_eq!(result.len(), 2);
+    assert!(result.contains(&partial_solution1));
+    assert!(result.contains(&partial_solution2));
+
+    let result = storage
+        .get_partial_solution(&ContentAddress(hash(&partial_solution1.data)))
+        .await
+        .unwrap()
+        .unwrap();
+    assert_eq!(result, partial_solution1);
+
+    storage
+        .move_partial_solutions_to_solved(&[hash(&partial_solution1.data)])
+        .await
+        .unwrap();
+
+    let result = storage.list_partial_solutions_pool().await.unwrap();
+    assert_eq!(result.len(), 1);
+    assert!(result.contains(&partial_solution2));
+
+    let result = storage
+        .is_partial_solution_solved(&ContentAddress(hash(&partial_solution1.data)))
+        .await
+        .unwrap()
+        .unwrap();
+    assert!(result);
+    let result = storage
+        .is_partial_solution_solved(&ContentAddress(hash(&partial_solution2.data)))
+        .await
+        .unwrap()
+        .unwrap();
+    assert!(!result);
+
+    storage
+        .insert_partial_solution_into_pool(partial_solution3.clone())
+        .await
+        .unwrap();
+
+    storage
+        .move_partial_solutions_to_solved(&[
+            hash(&partial_solution2.data),
+            hash(&partial_solution3.data),
+        ])
+        .await
+        .unwrap();
+
+    let result = storage
+        .is_partial_solution_solved(&ContentAddress(hash(&partial_solution3.data)))
+        .await
+        .unwrap()
+        .unwrap();
+    assert!(result);
+
+    let result = storage.list_partial_solutions_pool().await.unwrap();
+    assert!(result.is_empty());
+
+    let result = storage
+        .get_partial_solution(&ContentAddress(hash(&partial_solution2.data)))
+        .await
+        .unwrap()
+        .unwrap();
+    assert_eq!(result, partial_solution2);
+
+    let result = storage
+        .get_partial_solution(&ContentAddress(hash(&partial_solution2.data)))
+        .await
+        .unwrap()
+        .unwrap();
+    assert_eq!(result, partial_solution2);
+}
+
+#[tokio::test]
+async fn test_update_and_query_state() {
+    let storage = MemoryStorage::new();
+
+    let intent_set = sign_with_random_keypair(vec![intent_with_vars(0)]);
+    let address = ContentAddress(hash(&intent_set.data));
+    let key = [0; 4];
+    let word = Some(42);
+
+    // Test updating the state without an intent set
+    storage
+        .update_state(&address, &key, word)
+        .await
+        .unwrap_err();
+
+    // Test querying the state
+    let query_result = storage.query_state(&address, &key).await.unwrap();
+    assert_eq!(query_result, None);
+
+    storage
+        .insert_intent_set(StorageLayout {}, intent_set.clone())
+        .await
+        .unwrap();
+
+    // Test updating the state
+    let old = storage.update_state(&address, &key, word).await.unwrap();
+    assert_eq!(old, None);
+
+    // Test querying the state
+    let query_result = storage.query_state(&address, &key).await.unwrap();
+    assert_eq!(query_result, word);
+
+    // Test updating the state
+    let old = storage.update_state(&address, &key, Some(1)).await.unwrap();
+    assert_eq!(old, word);
+
+    // Test querying the state
+    let query_result = storage.query_state(&address, &key).await.unwrap();
+    assert_eq!(query_result, Some(1));
+
+    // Test querying empty state
+    let query_result = storage.query_state(&address, &[1; 4]).await.unwrap();
+    assert_eq!(query_result, None);
 }
---
+++ crates/memory-storage/src/values.rs
@@ -0,0 +1,81 @@
+use std::{
+    collections::{BTreeMap, HashMap},
+    ops::Range,
+    time::Duration,
+};
+
+use essential_types::{intent::Intent, Block, ContentAddress};
+
+use crate::IntentSet;
+
+#[cfg(test)]
+mod tests;
+
+pub fn page_intents<'a>(
+    intent_hashes: impl Iterator<Item = &'a ContentAddress>,
+    intents: &HashMap<ContentAddress, IntentSet>,
+    page: usize,
+    page_size: usize,
+) -> Vec<Vec<Intent>> {
+    let start = page * page_size;
+    intent_hashes
+        .skip(start)
+        .filter_map(|v| {
+            let set = intents.get(v)?;
+            Some(
+                set.order
+                    .iter()
+                    .filter_map(|i| set.data.get(i).cloned())
+                    .collect(),
+            )
+        })
+        .take(page_size)
+        .collect()
+}
+
+pub fn page_intents_by_time(
+    intent_times: &BTreeMap<Duration, ContentAddress>,
+    intents: &HashMap<ContentAddress, IntentSet>,
+    range: Range<Duration>,
+    page: usize,
+    page_size: usize,
+) -> Vec<Vec<Intent>> {
+    let start = page * page_size;
+    intent_times
+        .range(range)
+        .skip(start)
+        .filter_map(|(_, v)| {
+            let set = intents.get(v)?;
+            Some(
+                set.order
+                    .iter()
+                    .filter_map(|i| set.data.get(i).cloned())
+                    .collect(),
+            )
+        })
+        .take(page_size)
+        .collect()
+}
+
+pub fn page_winning_blocks(
+    blocks: &BTreeMap<Duration, Block>,
+    range: Option<Range<Duration>>,
+    page: usize,
+    page_size: usize,
+) -> Vec<Block> {
+    let start = page * page_size;
+    match range {
+        Some(range) => blocks
+            .range(range)
+            .skip(start)
+            .take(page_size)
+            .map(|(_, v)| v.clone())
+            .collect(),
+        None => blocks
+            .iter()
+            .skip(start)
+            .take(page_size)
+            .map(|(_, v)| v.clone())
+            .collect(),
+    }
+}
