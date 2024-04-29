6ab01d7cb2c3031b19f2bbdc18a526009e052c10
Tom <tomrgowan@gmail.com>
2024-04-29 13:22:36 +1000

    add tests, main, and nix

--- pkgs/essential-rest-server.nix
+++ pkgs/essential-rest-server.nix
@@ -1,25 +1,25 @@
-# A derivation for the `essential-server` crate.
-{ openssl
+# A derivation for the `essential-rest-server` crate.
+{ lib
+, stdenv
+, openssl
 , pkg-config
 , rustPlatform
+, darwin
 }:
 let
-  src = ./.;
-  crateDir = "${src}/crates/essential";
+  src = ../.;
+  crateDir = "${src}/crates/server";
   crateTOML = "${crateDir}/Cargo.toml";
   lockFile = "${src}/Cargo.lock";
 in
 rustPlatform.buildRustPackage {
   inherit src;
-  pname = "essential-server";
+  pname = "essential-rest-server";
   version = (builtins.fromTOML (builtins.readFile crateTOML)).package.version;
 
-  nativeBuildInputs = [
-    pkg-config
-  ];
-
   buildInputs = [
-    openssl
+  ] ++ lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.SystemConfiguration
   ];
 
   cargoLock = {
