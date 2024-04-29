f001edbf814ed5aa1c9395a96a0fcd28d2683f04
mitchmindtree <mail@mitchellnordine.com>
2024-04-30 07:43:42 +1000

    refactor: Add server-with-rqlite without flake-utils (#79)
    * refactor: Add server-with-rqlite without flake-utils
    
    Here's an alternative way of declaring `server-with-rqlite` using the
    existing setup that doesn't require introducing flake-utils but achieves
    the same result.
    
    Also rms some unused inputs from the `essential-rest-server` derivation
    and avoids running all of its tests after building the derivation as we
    already run them in CI.
    
    * fix: replace incorrect binary exe name yurtc with server-with-rqlite

---
+++ pkgs/server-with-rqlite.nix
@@ -0,0 +1,32 @@
+{ essential-rest-server
+, rqlite
+, writeShellApplication
+}:
+writeShellApplication {
+  name = "server-with-rqlite";
+
+  runtimeInputs = [ essential-rest-server rqlite ];
+
+  text = ''
+    # Check if an argument is provided
+    if [ "$#" -eq 0 ]; then
+      echo "Usage: $0 <rqlite-file-location>"
+      exit 1
+    fi
+
+    # Start rqlite in the background
+    rqlited -node-id=1 "$1" > /dev/null 2>&1 &
+    pid=$!  # Capture the PID of the last job run in the background
+
+    # Set a trap to kill the background process when the script exits
+    trap 'kill $pid 2> /dev/null' EXIT
+
+    # Wait for rqlite to start
+    sleep 2
+
+    essential-rest-server --db rqlite --rqlite-address http://localhost:4001
+
+    # Wait for the background process to finish
+    wait $pid
+  '';
+}
